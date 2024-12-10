581b6a416e9510b17d84935d71b0f5385add6bf9
TemirovaAzima <temirovaazima9@gmail.com>
2024-12-10 13:43:30 +0500

    Big O notation = O(1)

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-
-const products = ['tuxum', 'non', 'strawberry', 'sut', 'banan', 'tuxum', 'non', 'strawberry', 'sut', 'banan','tuxum', 'non', 'strawberry', 'sut', 'banan','tuxum', 'non', 'strawberry', 'sut', 'banan','tuxum', 'non', 'strawberry', 'sut', 'banan','apple']
-let startTime = new Date()
-
-const kerakliMaxsulotlar=(maxsulot)=>{
-    for(let i =0 ; i<products.length; i++){
-        if(products[i]=== maxsulot){
-            console.log(`Found PRODUCT ${maxsulot}`)
-
-        }
-    }
+const products= ['tuxum', 'non', 'qulupnay', 'sut', 'banan']
+const aynanBittaMaxsulotIzlash=(maxsulotlar, index)=>{
+    return maxsulotlar[index]
 }
-let endTime = new Date()
-
-kerakliMaxsulotlar('apple')
-console.log(`${endTime-startTime}`)jkm
-//  Big O = O(n)
-// const start = new Date()
-// console.log(start.getMinutes())
-// for(let i =0; i<1000000000; i++){
-//     let some= i**3
-// }
-// const end = new Date()
-// console.log(end)
-// const float = (end-start)/1000
-// console.log(`Loop ishladi ${parseFloat(float)} millisekund`)
+console.log(aynanBittaMaxsulotIzlash(products, 3))
+//B o notation = O(1)
